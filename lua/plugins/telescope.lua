a2a79d3b1c8a45503eaef28fa280a32283733eaa
Andreas <akemkers@gmail.com>
2025-02-18 11:55:38 +0000

    fjerner log linje som ikke trengs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -60,7 +60,6 @@ return {
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
-      print(vim.fn.system 'git rev-parse --show-toplevel', '\n', '')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
