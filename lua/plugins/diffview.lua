39893ce03f2c22763aee49048e0cc9f1c473c129
Andreas Foldvik Kemkers <andreas.foldvik.kemkers@sparebank1.no>
2025-08-06 13:28:48 +0200

    added keymaps to diff file and browse commit history

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,11 +1,25 @@
 return {
   'sindrets/diffview.nvim',
   opts = {
+    win_config = function()
+      local c = { type = 'float' }
+      local editor_width = vim.o.columns
+      local editor_height = vim.o.lines
+      c.width = math.min(100, editor_width)
+      c.height = math.min(24, editor_height)
+      c.col = math.floor(editor_width * 0.5 - c.width * 0.5)
+      c.row = math.floor(editor_height * 0.5 - c.height * 0.5)
+      return c
+    end,
+    enhanced_diff_hl = true,
     keymaps = {
       view = {
         { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Quit Diffview' } },
       },
       file_panel = {
+        { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Quit Diffview' } },
+      },
+      file_history_panel = {
         { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Quit Diffview' } },
       },
     },
@@ -33,6 +47,28 @@ return {
         end
       end,
       desc = 'Open Diffview (main)',
+    },
+    {
+      '<leader>gh',
+      function()
+        if next(require('diffview.lib').views) == nil then
+          vim.cmd 'DiffviewFileHistory'
+        else
+          vim.cmd 'DiffviewClose'
+        end
+      end,
+      desc = 'Open History',
+    },
+    {
+      '<leader>gt',
+      function()
+        if next(require('diffview.lib').views) == nil then
+          vim.cmd 'DiffviewFileHistory %'
+        else
+          vim.cmd 'DiffviewClose'
+        end
+      end,
+      desc = 'Open History for current file',
     },
   },
 }
