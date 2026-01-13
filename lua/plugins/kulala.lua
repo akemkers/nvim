f49025ca9d68c3b332cac06ef7d7ec3a99934fc9
Andreas Foldvik Kemkers <andreas.foldvik.kemkers@sparebank1.no>
2026-01-13 13:15:42 +0100

    fix timing issue with kulala and nvim, and map sb1 content types

--- lua/plugins/kulala.lua
+++ lua/plugins/kulala.lua
@@ -10,5 +10,9 @@ return {
     global_keymaps = true,
     global_keymaps_prefix = '<leader>R',
     kulala_keymaps_prefix = '',
+    contenttypes = {
+      ['application/vnd.sparebank1.v1+json'] = 'application/json',
+      ['application/vnd.sparebank1.v1+json;charset=utf-8'] = 'application/json',
+    },
   },
 }
--- lua/plugins/mini/files.lua
+++ lua/plugins/mini/files.lua
@@ -15,6 +15,14 @@ vim.api.nvim_create_autocmd('User', {
   callback = function(args)
     local buf_id = args.data.buf_id
 
+    -- Schedule to run after kulala's autocmds have set their keymaps
+    vim.schedule(function()
+      pcall(vim.keymap.del, 'n', '<CR>', { buffer = buf_id })
+      vim.keymap.set('n', '<CR>', function()
+        MiniFiles.go_in { close_on_file = true }
+      end, { buffer = buf_id, desc = 'Open file and close' })
+    end)
+
     vim.keymap.set('n', 'gc', function()
       local fs_entry = MiniFiles.get_fs_entry()
       if not fs_entry then
